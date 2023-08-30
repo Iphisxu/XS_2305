23dba24dcca5e3270413db34e9a13bd60d2a1b6f
Evan <iphisxu@gmail.com>
2023-08-30 15:49:49 +0800

    基础绘图

--- src/findpoint.py
+++ src/findpoint.py
@@ -1,64 +1,51 @@
 import numpy as np
-import pandas as pd
-import xarray as xr
 
-def findpoint(in_lon, in_lat, gridfile):
+def findpoint(in_lon, in_lat, ncfile):
     """
     in_lon: longitude of the station
     in_lat: latitude of the station
-    gridfile: xarray dataarray that contains coordinates 'longitude' and 'latitude'
+    ncfile: xarray dataarray that contains coordinates 'longitude' and 'latitude'
     nearest_point: output of the dataarray
     """
 
     # Compute the distances between each grid point and the specified [lon, lat] location
-    distances = ((gridfile.longitude.astype(np.float64) - float(in_lon))**2 +
-                (gridfile.latitude.astype(np.float64) - float(in_lat))**2)**0.5
+    distances = ((ncfile.longitude - in_lon)**2 + (ncfile.latitude - in_lat)**2)**0.5
+
     # Find the minimum distance and corresponding index in the flattened array
     min_distance = distances.min()
     min_index = distances.argmin()
 
     # Convert the flattened index to 2D index
-    # y_index, x_index = np.unravel_index(min_index, gridfile.data_vars[list(gridfile.data_vars.keys())[0]].shape)
-    y_index, x_index = np.unravel_index(min_index, gridfile.to_array()[0].shape)
+    y_index, x_index = np.unravel_index(min_index, ncfile[0,0,:,:].shape)
 
     # Get the value of the DataArray at the nearest grid point
-    # nearest_point = gridfile.isel(x=x_index, y=y_index)
+    # nearest_point = ncfile.isel(x=x_index, y=y_index)
     
     return x_index, y_index
 
 
-def assign_values_to_grid(input_table, gridfile, lon_col, lat_col, value_cols):
+
+def findpoint_test(in_lon, in_lat, nlon, nlat):
     """
-    input_table: pandas DataFrame containing columns 'lon_col', 'lat_col', and value_cols
-    gridfile: xarray Dataset with coordinates 'longitude' and 'latitude'
-    lon_col: column name for longitude in the input_table
-    lat_col: column name for latitude in the input_table
-    value_cols: list of column names for values in the input_table
-
-    Returns a new xarray Dataset with assigned values to the nearest grid points for each variable.
+    in_lon: longitude of the station
+    in_lat: latitude of the station
+    nlon: longitude of the model domain (2D Var)
+    nlat: latitude of the model domain (2D Var)
+    out_x: the nearest position of the station in the model domain in x direction
+    out_y: the nearest position of the station in the model domain in y direction
     """
-
-    # Create an empty dataset with the same coordinate information as gridfile
-    new_dataset = xr.Dataset(coords=gridfile.coords)
-
-    # Iterate over each value column
-    for col in value_cols:
-        # Create a new variable with the specified name
-        new_variable = np.zeros_like(gridfile.longitude)  # Initialize with zeros
-
-        # Iterate over each row in the input table
-        for _, row in input_table.iterrows():
-            lon, lat, value = row[lon_col], row[lat_col], row[col]
-
-            # Find the nearest grid point
-            x_index, y_index = findpoint(lon, lat, gridfile)
-
-            # Add the value to the new variable at the nearest grid point
-            new_variable[y_index, x_index] += value
-
-        # Assign the new variable to the new dataset using the specified name
-        new_dataset[col] = (('y', 'x'), new_variable)
-        
-        print('Complete ' + f'{col}')
-
-    return new_dataset
+       
+    xnum, ynum = nlon.shape
+    gridnum = xnum * ynum
+    
+    nlon = nlon.reshape(gridnum, 1)
+    nlat = nlat.reshape(gridnum, 1)
+    
+    out_x = np.nan
+    out_y = np.nan
+    
+    index = np.argmin((nlon - in_lon) ** 2 + (nlat - in_lat) ** 2)
+    out_y = np.ceil((index + 1) / xnum)
+    out_x = index + 1 - xnum * (out_y - 1)
+    
+    return out_x, out_y
